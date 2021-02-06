aa440fbde94579923d88d48041dad74b70a9acd6
agastya-mishra1 <78002464+agastya-mishra1@users.noreply.github.com>
2021-02-06 11:59:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function text(){ 
+document.getElementById("Hobby").style.color="blue";   
+}
+function ichange(){ 
+document.getElementById("img1").src="https://www.rvcj.com/wp-content/uploads/2018/11/1542898366-virat-kohli-ap-2-600x450.jpg";   
+}
+function Para(){ 
+document.getElementById("line").style.color="purple"; document.getElementById("line").style.fontStyle="italic"; 
+}
+
